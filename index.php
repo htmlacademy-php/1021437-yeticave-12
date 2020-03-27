63c73b782c5561bce9da19c0d221d049656f07da
Bogdan Moiseenko <moiseenkob@gmail.com>
2020-03-27 18:51:04 +0300

    Добавил функцию округления

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+function format_sum($number = 0)
+{
+    $price = 0;
+    if (is_integer(intval($number))) {
+        $price = ceil($number);
+        $format_price = null;
+        if ($price >= 1000) {
+            $format_price = number_format($price, 0, ',', ' ');
+        }
+        return $format_price .  " ₽";
+    }
+    return $price.  " ₽";
+}
 $is_auth = rand(0, 1);
 $user_name = 'Bogdan'; // укажите здесь ваше имя
 $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
@@ -115,7 +128,7 @@ $ads_main_page = [
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$lot['price']?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?php echo format_sum($lot['price']); ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
